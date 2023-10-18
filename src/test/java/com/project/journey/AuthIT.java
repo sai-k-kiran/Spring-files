6fa9faa0b2eaa467b0ca93e5ef8e47353eb6aa5b
Saikiran <saikiran.7698@gmail.com>
2023-10-18 18:04:22 +0530

    AuthIT completed

--- src/test/java/com/project/journey/AuthIT.java
+++ src/test/java/com/project/journey/AuthIT.java
@@ -1,9 +1,11 @@
 package com.project.journey;
 
 import com.project.auth.AuthenticationRequest;
+import com.project.auth.AuthenticationResponse;
 import com.project.customer.CustomerDTO;
 import com.project.customer.CustomerRegistrationRequest;
 import com.project.customer.Gender;
+import com.project.jwt.JWTutil;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +26,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AuthIT {
     @Autowired
     private WebTestClient webTestClient;
+
+    @Autowired
+    private JWTutil jwTutil;
+
     private static final Random RANDOM = new Random();
     private static final String CUSTOMER_PATH = "/api/v1/customers";
     private static final String AUTH_PATH = "/api/v1/auth";
@@ -32,15 +39,17 @@ public class AuthIT {
         Faker faker = new Faker();
         String name = faker.name().fullName();
         String email = name.toLowerCase().replaceAll(" ", "_") + "@gmail.com";
+        String password = "password";
+
         int age = RANDOM.nextInt(1, 100);
         Gender gender = (age % 2 == 0) ? Gender.MALE : Gender.FEMALE;
 
-        CustomerRegistrationRequest request =
-                new CustomerRegistrationRequest(name, age, email, gender, "password");
+        CustomerRegistrationRequest customerRegistrationRequest =
+                new CustomerRegistrationRequest(name, age, email, gender, password);
 
         AuthenticationRequest authenticationRequest = new AuthenticationRequest(
                 email,
-                "password"
+                password
         );
 
         webTestClient.post()
@@ -50,21 +59,52 @@ public class AuthIT {
                 .body(Mono.just(authenticationRequest), AuthenticationRequest.class)
                 .exchange()
                 .expectStatus()
+                .isUnauthorized();
+
+        webTestClient.post()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(customerRegistrationRequest), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
                 .isOk();
 
-//        String JWTtoken = webTestClient.post()
-//                .uri(CUSTOMER_PATH)
-//                .accept(MediaType.APPLICATION_JSON)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .body(Mono.just(request), CustomerRegistrationRequest.class)
-//                .exchange()
-//                .expectStatus()
-//                .isOk()
-//                .returnResult(Void.class)
-//                .getResponseHeaders()
-//                .get(HttpHeaders.AUTHORIZATION)
-//                .get(0);
+        WebTestClient.BodySpec<AuthenticationResponse, ?> spec = webTestClient.post()
+                .uri(AUTH_PATH + "/login")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(authenticationRequest), AuthenticationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<AuthenticationResponse>() {
+                });
 
+        EntityExchangeResult<AuthenticationResponse> result = webTestClient.post()
+                .uri(AUTH_PATH + "/login")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(authenticationRequest), AuthenticationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk().
+                expectBody(new ParameterizedTypeReference<AuthenticationResponse>() {
+                })
+                .returnResult();
 
+        String token = result.getResponseHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+        AuthenticationResponse authenticationResponse = result.getResponseBody();
+        CustomerDTO customerDTO = authenticationResponse.customerDTO();
+
+        assertThat(jwTutil.isValidToken(token, customerDTO.username())).isTrue();
+
+        assertThat(customerDTO.email()).isEqualTo(email);
+        assertThat(customerDTO.age()).isEqualTo(age);
+        assertThat(customerDTO.name()).isEqualTo(name);
+        assertThat(customerDTO.username()).isEqualTo(email);
+        assertThat(customerDTO.gender()).isEqualTo(gender);
+        assertThat(customerDTO.roles()).isEqualTo(List.of("ROLE_USER"));
     }
 }
